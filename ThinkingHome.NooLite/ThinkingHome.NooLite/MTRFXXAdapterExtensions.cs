cd48f498b56da851f2e46669b0adbab5d98520a3
Dmitry Andriyanov <dima117a@yandex-team.ru>
2017-09-12 20:48:44 +0300

    rx + tx implementation

--- ThinkingHome.NooLite/ThinkingHome.NooLite/MTRFXXAdapterExtensions.cs
+++ ThinkingHome.NooLite/ThinkingHome.NooLite/MTRFXXAdapterExtensions.cs
@@ -2,29 +2,105 @@
 {
     public static class MTRFXXAdapterExtensions
     {
+        private static void Send(MTRFXXAdapter adapter, MTRFXXCommand command, byte channel = 0)
+        {
+            adapter.SendCommand(MTRFXXMode.TX, MTRFXXAction.SendCommand, channel, command);
+        }
+
+        private static void SendData(MTRFXXAdapter adapter, MTRFXXCommand command, byte channel, MTRFXXDataFormat format, params byte[] data)
+        {
+            adapter.SendCommand(MTRFXXMode.TX, MTRFXXAction.SendCommand, channel, command, MTRFXXRepeatCount.NoRepeat, format, data);
+        }
+        
         // brightness
-        public static void On(this MTRFXXAdapter adapter, int channel) { }
-        public static void Off(this MTRFXXAdapter adapter, int channel) { }
-        public static void Toggle(this MTRFXXAdapter adapter, int channel) { }
-        public static void SetBrightness(this MTRFXXAdapter adapter, int channel, int brightness) { }
+        public static void On(this MTRFXXAdapter adapter, byte channel)
+        {
+            Send(adapter, MTRFXXCommand.On, channel);
+        }
+
+        public static void Off(this MTRFXXAdapter adapter, byte channel)
+        {
+            Send(adapter, MTRFXXCommand.Off, channel);
+        }
+
+        public static void Toggle(this MTRFXXAdapter adapter, byte channel)
+        {
+            Send(adapter, MTRFXXCommand.Switch, channel);
+        }
+
+        public static void SetBrightness(this MTRFXXAdapter adapter, byte channel, byte brightness)
+        {
+            SendData(adapter, MTRFXXCommand.SetBrightness, channel, MTRFXXDataFormat.OneByteData, brightness);
+        }
 
         // presets
-        public static void SavePreset(this MTRFXXAdapter adapter, int channel) { }
-        public static void LoadPreset(this MTRFXXAdapter adapter, int channel) { }
+        public static void SavePreset(this MTRFXXAdapter adapter, byte channel)
+        {
+            Send(adapter, MTRFXXCommand.SavePreset, channel);    
+        }
+
+        public static void LoadPreset(this MTRFXXAdapter adapter, byte channel)
+        {
+            Send(adapter, MTRFXXCommand.LoadPreset, channel);
+        }
 
         // led
-        public static void SetLedColor(this MTRFXXAdapter adapter, int channel, byte valueR, byte valueG, byte valueB) { }
-        public static void ChangeLedColor(this MTRFXXAdapter adapter, int channel) { }
-        public static void ChangeLedColorMode(this MTRFXXAdapter adapter, int channel) { }
-        public static void ChangeLedColorSpeed(this MTRFXXAdapter adapter, int channel) { }
+        public static void SetLedColor(this MTRFXXAdapter adapter, byte channel, byte valueR, byte valueG, byte valueB)
+        {
+            SendData(adapter, MTRFXXCommand.SetBrightness, channel, MTRFXXDataFormat.LED, valueR, valueG, valueB);
+        }
+
+        public static void ChangeLedColor(this MTRFXXAdapter adapter, byte channel)
+        {
+            Send(adapter, MTRFXXCommand.Switch_Colour, channel);
+        }
+
+        public static void ChangeLedColorMode(this MTRFXXAdapter adapter, byte channel)
+        {
+            Send(adapter, MTRFXXCommand.Switch_Mode, channel);
+        }
+
+        public static void ChangeLedColorSpeed(this MTRFXXAdapter adapter, byte channel)
+        {
+            Send(adapter, MTRFXXCommand.Speed_Mode_Back, channel);
+        }
 
         // binding tx
-        public static void Bind(this MTRFXXAdapter adapter, int channel) { }
-        public static void UnBind(this MTRFXXAdapter adapter, int channel) { }
+        public static void Bind(this MTRFXXAdapter adapter, byte channel)
+        {
+            Send(adapter, MTRFXXCommand.Bind, channel);
+        }
+
+        public static void UnBind(this MTRFXXAdapter adapter, byte channel)
+        {
+            Send(adapter, MTRFXXCommand.Unbind, channel);
+        }
         
         // binding rx
-        public static void BindStart(this MTRFXXAdapter adapter) { }
-        public static void BindStop(this MTRFXXAdapter adapter) { }
-        public static void UnBindAll(this MTRFXXAdapter adapter) { }                
+        public static void BindStart(this MTRFXXAdapter adapter, byte channel)
+        {
+            adapter.SendCommand(MTRFXXMode.RX, MTRFXXAction.StartBinding, channel, MTRFXXCommand.None);
+        }
+
+        public static void BindStop(this MTRFXXAdapter adapter)
+        {
+            adapter.SendCommand(MTRFXXMode.RX, MTRFXXAction.StopBinding, 0, MTRFXXCommand.None);
+        }
+
+        public static void ClearChannel(this MTRFXXAdapter adapter, byte channel)
+        {
+            adapter.SendCommand(MTRFXXMode.RX, MTRFXXAction.ClearChannel, channel, MTRFXXCommand.None);
+        }
+
+        public static void ClearAllChannels(this MTRFXXAdapter adapter)
+        {
+            adapter.SendCommand(MTRFXXMode.RX, MTRFXXAction.ClearAllChannels, 0, MTRFXXCommand.None, 
+                MTRFXXRepeatCount.NoRepeat, MTRFXXDataFormat.NoData, new byte[] { 170, 85, 170, 85 });
+        }
+
+        public static void ExitServiceMode(this MTRFXXAdapter adapter)
+        {
+            adapter.SendCommand(MTRFXXMode.Service, MTRFXXAction.SendCommand, 0, MTRFXXCommand.None);
+        }   
     }
 }
--- ThinkingHome.NooLite/ThinkingHome.NooLite/MTRFXXCommand.cs
+++ ThinkingHome.NooLite/ThinkingHome.NooLite/MTRFXXCommand.cs
@@ -2,6 +2,8 @@
 {
     public enum MTRFXXCommand : byte
     {
+        None = 0,
+        
         Off = 0,
         BrightnessDown = 1,
         On = 2,
@@ -14,12 +16,17 @@
         SetBrightness = 6,
         LoadPreset = 7,
         SavePreset = 8,
-        Unbind = 9,
         BrightnessStop = 10,
         BrightnessStepDown = 11,
         BrightnessStepUp = 12,
         BrightnessStart = 13,
 
-        Bind = 15
+        Bind = 15,
+        Unbind = 9,
+        
+        Roll_Colour = 16,
+        Switch_Colour = 17,
+        Switch_Mode = 18,
+        Speed_Mode_Back = 19
     }
 }
