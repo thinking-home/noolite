e8800d31c39346c493fa0666d498b0587b42cc25
Dmitry Andriyanov <dima117a@yandex-team.ru>
2017-09-12 10:41:49 +0300

    adapter API

---
+++ ThinkingHome.NooLite/ThinkingHome.NooLite/MTRFXXAdapterExtensions.cs
@@ -0,0 +1,13 @@
+﻿namespace ThinkingHome.NooLite
+{
+    public static class MTRFXXAdapterExtensions
+    {
+        public static void On(this MTRFXXAdapter adapter, int channel) { }
+        public static void Off(this MTRFXXAdapter adapter, int channel) { }
+        public static void SetBrightness(this MTRFXXAdapter adapter, int channel, int brightness) { }
+        public static void Bind(this MTRFXXAdapter adapter, int channel) { }
+        public static void UnBind(this MTRFXXAdapter adapter, int channel) { }
+        public static void SetLedColor(this MTRFXXAdapter adapter, int channel, byte valueR, byte valueG, byte valueB) { }
+        
+    }
+}
