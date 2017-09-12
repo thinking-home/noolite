eef7be102f55fdf8aa44b686d7e47e429b5d022e
Dmitry Andriyanov <dima117a@yandex-team.ru>
2017-09-12 12:19:14 +0300

    interface

--- ThinkingHome.NooLite/ThinkingHome.NooLite/MTRFXXAdapterExtensions.cs
+++ ThinkingHome.NooLite/ThinkingHome.NooLite/MTRFXXAdapterExtensions.cs
@@ -2,12 +2,29 @@
 {
     public static class MTRFXXAdapterExtensions
     {
+        // brightness
         public static void On(this MTRFXXAdapter adapter, int channel) { }
         public static void Off(this MTRFXXAdapter adapter, int channel) { }
+        public static void Toggle(this MTRFXXAdapter adapter, int channel) { }
         public static void SetBrightness(this MTRFXXAdapter adapter, int channel, int brightness) { }
+
+        // presets
+        public static void SavePreset(this MTRFXXAdapter adapter, int channel) { }
+        public static void LoadPreset(this MTRFXXAdapter adapter, int channel) { }
+
+        // led
+        public static void SetLedColor(this MTRFXXAdapter adapter, int channel, byte valueR, byte valueG, byte valueB) { }
+        public static void ChangeLedColor(this MTRFXXAdapter adapter, int channel) { }
+        public static void ChangeLedColorMode(this MTRFXXAdapter adapter, int channel) { }
+        public static void ChangeLedColorSpeed(this MTRFXXAdapter adapter, int channel) { }
+
+        // binding tx
         public static void Bind(this MTRFXXAdapter adapter, int channel) { }
         public static void UnBind(this MTRFXXAdapter adapter, int channel) { }
-        public static void SetLedColor(this MTRFXXAdapter adapter, int channel, byte valueR, byte valueG, byte valueB) { }
         
+        // binding rx
+        public static void BindStart(this MTRFXXAdapter adapter) { }
+        public static void BindStop(this MTRFXXAdapter adapter) { }
+        public static void UnBindAll(this MTRFXXAdapter adapter) { }                
     }
 }
--- ThinkingHome.NooLite/ThinkingHome.NooLite/MTRFXXDataFormat.cs
+++ ThinkingHome.NooLite/ThinkingHome.NooLite/MTRFXXDataFormat.cs
@@ -2,6 +2,9 @@
 {
     public enum MTRFXXDataFormat : byte
     {
-        NoData = 0
+        NoData = 0x00,
+        OneByteData = 0x01,
+        FourByteData = 0x03,
+        LED = 0x04
     }
 }
