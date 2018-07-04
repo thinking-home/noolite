13916e110f2a68b3378383fe10ac7426c95b7f4b
Dmitry Andriyanov <dima117a@yandex-team.ru>
2018-07-04 09:35:42 +0300

    заготовка консольной утилиты

--- ThinkingHome.NooLite.Console/Program.cs
+++ ThinkingHome.NooLite.Console/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Reflection;
+using McMaster.Extensions.CommandLineUtils;
 
 namespace ThinkingHome.NooLite.Console
 {
@@ -6,7 +8,35 @@ namespace ThinkingHome.NooLite.Console
     {
         static void Main(string[] args)
         {
-            System.Console.WriteLine("Hello World!");
+            var ver = Assembly.GetExecutingAssembly().GetName().Version;
+
+            var app = new CommandLineApplication();
+            app.Name = "noolite";
+            app.Description = $"nooLite command line interface - v{ver.Major}.{ver.Minor}.{ver.Revision}";
+            app.HelpOption("-?|-h|--help");
+            app.ExtendedHelpText = "\nSee the details on https://github.com/thinking-home/noolite#readme.";
+
+            var port = app.Option<string>("-p|--port", "Serial port name which nooLite adapter connected to", CommandOptionType.SingleValue);
+            var chanel = app.Option<int>("-c|--channel", "Target channel", CommandOptionType.SingleValue);
+
+            app.Command("on", cmd => { cmd.Description = "123"; });
+            app.Command("off", cmd => { cmd.Description = "456"; });
+
+            app.OnExecute(() =>
+            {
+                System.Console.WriteLine($"executed: {port.ParsedValue}, channel: {chanel.ParsedValue}");
+            });
+
+            try
+            {
+                app.Execute(args);
+            }
+            catch (CommandParsingException e)
+            {
+                System.Console.Error.WriteLine(e.Message);
+                app.ShowHelp();
+                Environment.ExitCode = 1;
+            }
         }
     }
 }
