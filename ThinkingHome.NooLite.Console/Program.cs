6f645c76c6c170cdea3345f76a57b995c4e9f7ec
Dmitry Andriyanov <dima117a@yandex-team.ru>
2018-07-04 22:58:15 +0300

    описано часть коамнд CLI

--- ThinkingHome.NooLite.Console/Program.cs
+++ ThinkingHome.NooLite.Console/Program.cs
@@ -6,6 +6,29 @@ namespace ThinkingHome.NooLite.Console
 {
     class Program
     {
+        private static CommandOption<string> optionPort;
+        private static CommandOption<byte> optionChannel;
+        private static CommandOption optionMode;
+
+        static void Invoke(Action<MTRFXXAdapter, byte> action, Action<MTRFXXAdapter, byte> actionF)
+        {
+            using (var adapter = new MTRFXXAdapter(optionPort.ParsedValue))
+            {
+                adapter.Open();
+                adapter.ExitServiceMode();
+
+
+                if (optionMode.HasValue())
+                {
+                    actionF(adapter, optionChannel.ParsedValue);
+                }
+                else
+                {
+                    action(adapter, optionChannel.ParsedValue);
+                }
+            }
+        }
+
         static void Main(string[] args)
         {
             var ver = Assembly.GetExecutingAssembly().GetName().Version;
@@ -16,15 +39,38 @@ namespace ThinkingHome.NooLite.Console
             app.HelpOption("-?|-h|--help");
             app.ExtendedHelpText = "\nSee the details on https://github.com/thinking-home/noolite#readme.";
 
-            var port = app.Option<string>("-p|--port", "Serial port name which nooLite adapter connected to", CommandOptionType.SingleValue);
-            var chanel = app.Option<int>("-c|--channel", "Target channel", CommandOptionType.SingleValue);
+            optionPort = app.Option<string>("-p|--port", "Serial port name which nooLite adapter connected to.", CommandOptionType.SingleValue);
+            optionChannel = app.Option<byte>("-c|--channel", "Target channel.", CommandOptionType.SingleValue);
+            optionMode = app.Option("-f", "Set the nooLite-F mode.", CommandOptionType.NoValue);
 
-            app.Command("on", cmd => { cmd.Description = "123"; });
-            app.Command("off", cmd => { cmd.Description = "456"; });
-
-            app.OnExecute(() =>
+            app.Command("bind", cmd =>
             {
-                System.Console.WriteLine($"executed: {port.ParsedValue}, channel: {chanel.ParsedValue}");
+                cmd.Description = "123";
+                cmd.OnExecute(() => Invoke((a, c) => a.Bind(c), (a, c) => a.BindF(c)));
+            });
+
+            app.Command("unbind", cmd =>
+            {
+                cmd.Description = "123";
+                cmd.OnExecute(() => Invoke((a, c) => a.Unbind(c), (a, c) => a.UnbindF(c)));
+            });
+
+            app.Command("on", cmd =>
+            {
+                cmd.Description = "123";
+                cmd.OnExecute(() => Invoke((a, c) => a.On(c), (a, c) => a.OnF(c)));
+            });
+
+            app.Command("off", cmd =>
+            {
+                cmd.Description = "123";
+                cmd.OnExecute(() => Invoke((a, c) => a.Off(c), (a, c) => a.OffF(c)));
+            });
+
+            app.Command("switch", cmd =>
+            {
+                cmd.Description = "123";
+                cmd.OnExecute(() => Invoke((a, c) => a.Switch(c), (a, c) => a.SwitchF(c)));
             });
 
             try
