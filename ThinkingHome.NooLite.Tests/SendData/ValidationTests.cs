7471b73a233220625dc7bc6409b520bf7799165f
Vadim Kosin <vkosin@outlook.com>
2020-08-29 08:03:07 +0300

    update

---
+++ ThinkingHome.NooLite.Tests/SendData/ValidationTests.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Xunit;
+
+namespace ThinkingHome.NooLite.Tests.ReceivedData
+{
+    public class ValidationTests
+    {
+        public const int VALID_DATA_SIZE = 17;
+        
+        [Fact]
+        public void Constructor_ForNull_ThrowsException()
+        {
+            byte[] nullReceivedData = null;
+
+            Exception ex = Assert.ThrowsAny<Exception>(() => new NooLite.ReceivedData(nullReceivedData));
+            
+            Assert.Contains("null", ex.Message);
+        }
+
+        [Fact]
+        public void Constructor_ForInvalidDataSize_ThrowsException()
+        {
+            byte[] invalidSizeData = new byte[3];
+
+            Exception ex = Assert.ThrowsAny<Exception>(() => new NooLite.ReceivedData(invalidSizeData));
+            
+            Assert.Contains("length", ex.Message);
+        }
+
+        
+        [Fact]
+        public void Constructor_ForInvalidFirstByte_ThrowsException()
+        {
+            byte[] testData = new byte[VALID_DATA_SIZE];
+            testData[0] = 124;
+
+            Exception ex = Assert.ThrowsAny<Exception>(() => new NooLite.ReceivedData(testData));
+            
+            Assert.Contains("start", ex.Message);
+        }
+
+        [Fact]
+        public void Constructor_ForInvalidLastByte_ThrowsException()
+        {
+            byte[] testData = new byte[VALID_DATA_SIZE];
+            testData[0] = 173;    // valid start marker
+            testData[VALID_DATA_SIZE - 1] = 124;
+
+            Exception ex = Assert.ThrowsAny<Exception>(() => new NooLite.ReceivedData(testData));
+            
+            Assert.Contains("stop", ex.Message);
+        }
+    }
+}
